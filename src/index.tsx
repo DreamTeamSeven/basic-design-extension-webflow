7cd54c5d51d2a41ae0472e4fc91b82403067455d
Pmacdon15 <128737876+Pmacdon15@users.noreply.github.com>
2025-01-15 16:50:18 -0700

    added add element

--- src/index.tsx
+++ src/index.tsx
@@ -8,29 +8,24 @@ const App: React.FC = () => {
 
     // If text content can be set, update it!
     if (el && el.textContent) {
-      await el.setTextContent("hello world!");
+      await el.setTextContent("hello world");
     } else {
       alert("Please select a text element");
     }
   };
 
-  // const addElement = async () => {
-  //   // Get the current selected Element
-  //   const selectedElement = await webflow.getSelectedElement()
-  //   if (selectedElement) {
-  //     // Insert DIV after selected Element
-  //     const newDiv = await selectedElement.after(webflow.elementPresets.DivBlock)
+  const addElement = async () => {
+    // Get the current selected Element
+    const selectedElement = await webflow.getSelectedElement()
+    if (selectedElement) {
+      // Insert DIV after selected Element
+      const newDiv = await selectedElement.after(webflow.elementPresets.Heading)
 
-  //     // await newDiv?.append(webflow.elementPresets.DOM)
-  //     // if (newDiv&& newDiv.textContent) {
-  //     //   await newDiv.setCustomAttribute("newDiv", "New div added to the screen")
-  //     // }
-
-  //     console.log(`${JSON.stringify(newDiv)}`)
-  //   } else {
-  //     alert("Please select an element");
-  //   }
-  // };
+      console.log(`${JSON.stringify(newDiv)}`)
+    } else {
+      alert("Please select an element");
+    }
+  };
   
   // const addComponent = async () => {
   //   const rootElement = await webflow.getSelectedElement();
@@ -47,7 +42,7 @@ const App: React.FC = () => {
       <h1>Welcome to My React App</h1>
       <p>This is a basic React application</p>
       <button onClick={addText}> Add text </button>
-      {/* <button onClick={addElement}> Add element </button> */}
+      <button onClick={addElement}> Add element </button>
       {/* <button onClick={addComponent}> Add component </button> */}
     </div>
   );
